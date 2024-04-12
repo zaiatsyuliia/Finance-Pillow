5de43499c0741ad5e2c9c94c758541b364d5fc0a
Akinabisu <akinabisu@gmail.com>
2024-04-12 04:23:27 +0300

    X

--- App/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ App/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a375cacb84940fc7071f3c9fa478e2380a7a3d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21c440639612f78f74916e234477eccc6997671f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
